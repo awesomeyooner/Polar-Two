21037ddbe3382c6769034d91365f9dcb89e852b2
Susan Yoon <awesomeyooner@gmail.com>
2024-12-24 13:19:20 -0800

    changed effort

--- src/my_robot/src/util/EffortTranslator.cpp
+++ src/my_robot/src/util/EffortTranslator.cpp
@@ -7,6 +7,7 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
 #include "std_msgs/msg/float64_multi_array.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 using std::placeholders::_1;
 
@@ -16,9 +17,10 @@ class EffortTranslator : public rclcpp::Node
  
     EffortTranslator() : Node("translator")
     {
-        velocity_publisher = this->create_publisher<std_msgs::msg::Float64MultiArray>("velocity_controller/commands", 10);
-        position_publisher = this->create_publisher<std_msgs::msg::Float64MultiArray>("position_controller/commands", 10);
         
+        left_publisher = this->create_publisher<std_msgs::msg::Float64>("esp32/left_motor/command", 10);
+        right_publisher = this->create_publisher<std_msgs::msg::Float64>("esp32/right_motor/command", 10);
+
         subscription = this->create_subscription<geometry_msgs::msg::TwistStamped>(
         "cmd_vel", 10, std::bind(&EffortTranslator::topic_callback, this, _1));
     }
@@ -28,20 +30,20 @@ class EffortTranslator : public rclcpp::Node
     {
       //RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg.axes[0]);
 
-      std_msgs::msg::Float64MultiArray velocity_command = std_msgs::msg::Float64MultiArray();
-      std_msgs::msg::Float64MultiArray position_command = std_msgs::msg::Float64MultiArray();
+      std_msgs::msg::Float64 left_command = std_msgs::msg::Float64();
+      std_msgs::msg::Float64 right_command = std_msgs::msg::Float64();
 
-      velocity_command.data.push_back(msg.twist.linear.x);
-      position_command.data.push_back(msg.twist.angular.z);
-
-      velocity_publisher -> publish(velocity_command);
-      position_publisher -> publish(position_command);
+      left_command.data = (msg.twist.linear.x - (msg.twist.angular.z * 0.4));
+      right_command.data = (msg.twist.linear.x + (msg.twist.angular.z * 0.4));
+  
+      left_publisher->publish(left_command);
+      right_publisher->publish(right_command);
     }
 
     rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr subscription;
 
-    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr velocity_publisher;
-    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr position_publisher;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr left_publisher;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr right_publisher;
 };
 
 int main(int argc, char * argv[])

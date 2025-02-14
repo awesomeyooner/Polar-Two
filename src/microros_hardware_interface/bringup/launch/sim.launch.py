96ef28e4e82eca72f45fd48de1504459b82a3495
Susan Yoon <awesomeyooner@gmail.com>
2025-02-13 19:26:00 -0800

    migrated to ignition (gz)

--- src/microros_hardware_interface/bringup/launch/sim.launch.py
+++ src/microros_hardware_interface/bringup/launch/sim.launch.py
@@ -60,19 +60,56 @@ def generate_launch_description():
 
     external_controllers = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(get_package_share_directory(hardware_package), "bringup", "launch", 
-        "external_controllers.launch.py"))
+        "external_controllers.launch.py")),
+        launch_arguments={'use_sim_time': 'true'}.items()
     )
-    
+
+    default_world = os.path.join(
+        get_package_share_directory(hardware_package),
+        'bringup',
+        'worlds',
+        'obstacles.world'
+    )
+
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+        )
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_robot'],
+                                   '-name', 'my_robot',
+                                   '-z', '0.1'],
                         output='screen')
+    
+    bridge_params = os.path.join(get_package_share_directory(hardware_package), 'bringup', 'config','gz_bridge.yaml')
+
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
+    )
+
     
     return LaunchDescription([
         # microros_launch,
@@ -81,6 +118,10 @@ def generate_launch_description():
         robot_state_publisher_launch,
         external_controllers,
 
+        world_arg,
         gazebo,
-        spawn_entity
+        spawn_entity,
+
+        ros_gz_bridge,
+        ros_gz_image_bridge
     ])

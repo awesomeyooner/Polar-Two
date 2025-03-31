b51249ba92d5e77065f29b702e19a0b821070b92
Susan Yoon <awesomeyooner@gmail.com>
2025-03-30 22:09:55 -0700

    added odometry and kinematics (untested)

---
+++ src/differential_drive_controller/include/differential_drive_controller/util/differential_drive_drivetrain.hpp
@@ -0,0 +1,127 @@
+#ifndef DIFFERENTIAL_DRIVE_DRIVETRAIN_HPP
+#define DIFFERENTIAL_DRIVE_DRIVETRAIN_HPP
+
+#include "utility.hpp"
+#include "differential_drive_odometry.hpp"
+
+using namespace utility;
+
+class DifferentialDriveDrivetrain{
+
+    public:
+
+        DifferentialDriveOdometry odometry;
+
+        std::vector<WheelHandle> left_wheels;
+        std::vector<WheelHandle> right_wheels;
+
+        double wheel_radius;
+        double circumference;
+
+        DifferentialDriveDrivetrain(double track_width_, double wheel_radius_) : 
+            odometry(track_width_), wheel_radius(wheel_radius_), circumference(wheel_radius_ * M_PI * 2){}
+    
+        void initialize(std::vector<WheelHandle>& left, std::vector<WheelHandle>& right ){
+            left_wheels = left;
+            right_wheels = right;
+        }
+
+        void update(){
+            double left_positions = WheelHandle::get_position(left_wheels);
+            double right_positions = WheelHandle::get_position(right_wheels);
+
+            odometry.update(left_positions, right_positions);
+        }
+
+        // get members
+        DifferentialDriveOdometry* get_odometry(){
+            return &odometry;
+        }
+
+        DifferentialDriveKinematics* get_kinematics(){
+            return odometry.get_kinematics();
+        }
+
+        // get positions
+        double get_left_positions(){
+            return WheelHandle::get_position(left_wheels) * circumference;
+        }
+
+        double get_right_positions(){
+            return WheelHandle::get_position(right_wheels) * circumference;
+        }
+
+        WheelPositions get_wheel_positions(){
+            return WheelPositions(get_left_positions(), get_right_positions());
+        }
+
+        // get velocities
+        double get_left_velocities(){
+            return WheelHandle::get_velocity(left_wheels) * circumference;
+        }
+
+        double get_right_velocities(){
+            return WheelHandle::get_velocity(right_wheels) * circumference;
+        }
+
+        WheelSpeeds get_wheel_speeds(){
+            return WheelSpeeds(get_left_velocities(), get_right_velocities());
+        }
+
+        Twist to_chassis_speed(){
+            return get_kinematics()->toChassisSpeeds(get_wheel_speeds());
+        }
+
+        // controls
+        void drive_from_chassis(Twist desired){
+            //meters per second -> rotations per second
+            WheelSpeeds wheel_speeds = odometry.get_kinematics()->toWheelSpeeds(desired).times(1 / (circumference));
+
+            //set the wheels to the speeds
+            set_left_command(wheel_speeds.left_velocity);
+            set_right_command(wheel_speeds.right_velocity);
+        }
+
+        /**
+         * @brief Drives with values of the twist as percentages (-1, 1)
+         * 
+         * @param desired Desired chassis speed, each value represented as a percent from (-1, 1)
+         */
+        void drive_open_loop(Twist desired){
+            double linear = desired.dx;
+            double angular = desired.dTheta;
+
+            double angular_coef;
+
+            double left_speed = linear - (angular * angular_coef);
+            double right_speed = linear + (angular * angular_coef);
+
+            set_left_command(left_speed);
+            set_right_command(right_speed);
+        }
+
+        /**
+         * @brief Sets the speed in RPS
+         * 
+         * @param command Desired speed in RPS
+         */
+        void set_left_command(double command){
+            WheelHandle::set_command(command, left_wheels);
+        }
+
+        /**
+         * @brief Set the speed in RPS
+         * 
+         * @param command Desired speed in RPS
+         */
+        void set_right_command(double command){
+            WheelHandle::set_command(command, right_wheels);
+        }
+
+    private:
+
+
+
+};
+
+#endif //DIFFERENTIAL_DRIVE_DRIVETRAIN_HPP

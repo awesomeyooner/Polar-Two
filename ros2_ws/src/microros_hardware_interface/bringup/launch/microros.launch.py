71c4e1bff06b8e88a11bd966a0d172434e06add4
Susan Yoon <awesomeyooner@gmail.com>
2025-04-05 13:27:23 -0700

    seperated workspaces

--- ros2_ws/src/microros_hardware_interface/bringup/launch/microros.launch.py
+++ ros2_ws/src/microros_hardware_interface/bringup/launch/microros.launch.py
@@ -1,6 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from launch.actions import DeclareLaunchArgument
 from launch.actions import ExecuteProcess
 import launch
@@ -12,12 +14,24 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
 
     return launch.LaunchDescription([
-        
-        launch_ros.actions.Node(
-            package='micro_ros_agent',
-            executable='micro_ros_agent',
-            name='micro_ros_agent_node',
-            arguments=['serial', '--dev', '/dev/ttyUSB0'],
-            output='screen',
+
+        ExecuteProcess(
+            cmd=[PathJoinSubstitution([
+                    FindPackageShare('microros_hardware_interface'),
+                    'bringup',
+                    'scripts',
+                    'launch_microros.sh'
+                ])],
+            shell=True,
+            name='source_script',
+            output='screen'
         ),
+        
+        # launch_ros.actions.Node(
+        #     package='micro_ros_agent',
+        #     executable='micro_ros_agent',
+        #     name='micro_ros_agent_node',
+        #     arguments=['serial', '--dev', '/dev/ttyUSB0'],
+        #     output='screen',
+        # ),
   ])

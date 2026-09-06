3af71bf8c1760484094507be4dcde5b2b3de6d25
Aaron Yoon <awesomeyooner@gmail.com>
2026-09-06 10:43:38 -0700

    changed pulllup to no pullup

--- firmware/cube/Core/Src/gpio.c
+++ firmware/cube/Core/Src/gpio.c
@@ -62,13 +62,13 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PA15 */
   GPIO_InitStruct.Pin = GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PC10 PC11 PC12 */
   GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
--- firmware/src/core.cpp
+++ firmware/src/core.cpp
@@ -4,9 +4,12 @@
 
 #include "EmbeddedLib/devices/gpio_device.hpp"
 
+#include "WireLib/communication/protocols/serial_interface.hpp"
+
 
 GPIODevice led = GPIODevice(GPIOC, GPIO_PIN_1);
 
+int bob = 0;
 
 void init()
 {
@@ -16,7 +19,9 @@ void init()
 
 void update()
 {
+    Serial.println(bob);
 
+    HAL_Delay(500);
     // led.set_high();
     // HAL_Delay(500);
     // led.set_low();
@@ -29,6 +34,8 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
     if(GPIO_Pin == GPIO_PIN_11)
         led.set_high();
-    else if(GPIO_Pin == GPIO_PIN_12)
-        led.set_low();
+
+    bob++;
+    // else if(GPIO_Pin == GPIO_PIN_12)
+        // led.set_low();
 }

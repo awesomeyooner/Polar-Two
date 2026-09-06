869230e56e2b56188f67771fc89cc930894444f3
Aaron Yoon <awesomeyooner@gmail.com>
2026-09-06 10:35:31 -0700

    added simple blink

--- firmware/src/core.cpp
+++ firmware/src/core.cpp
@@ -1,5 +1,12 @@
 #include "core.hpp"
 
+#include "gpio.h"
+
+#include "EmbeddedLib/devices/gpio_device.hpp"
+
+
+GPIODevice led = GPIODevice(GPIOC, GPIO_PIN_1);
+
 
 void init()
 {
@@ -9,5 +16,19 @@ void init()
 
 void update()
 {
+
+    // led.set_high();
+    // HAL_Delay(500);
+    // led.set_low();
+    // HAL_Delay(500);
     
 } // end of "update()"
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if(GPIO_Pin == GPIO_PIN_11)
+        led.set_high();
+    else if(GPIO_Pin == GPIO_PIN_12)
+        led.set_low();
+}
